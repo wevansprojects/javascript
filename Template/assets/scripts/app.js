93f4c4bcb5a45e9e0098e48d52c53f3e6028c4c9
William Evans <williamjevans@hotmail.com>
2023-03-31 13:45:42 +0800

    latest

--- Template/assets/scripts/app.js
+++ Template/assets/scripts/app.js
@@ -1,1 +1,5 @@
-alert('This Works');
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult = (currentResult + 10) * 3 /2 -1;
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+outputResult(currentResult, calculationDescription);
