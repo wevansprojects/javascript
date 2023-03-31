fc38f04a6f6768fefbf95d3d08fdebc89aebe551
William Evans <williamjevans@hotmail.com>
2023-03-31 13:46:13 +0800

    latest

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+const userInput = 2;
+let result;
+result = 18 + userInput;
+result = result - 2;
+result = result * 2;
+result = result / 4;
+alert("The result is: " + result);
+alert("The userInput value is: " + userInput);
+
